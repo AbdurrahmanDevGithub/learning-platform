9e85c7da1441218c82d8dd6dc8c25b74d11098c8
Abdurrahman <arhmn1905@gmail.com>
2024-11-17 17:55:18 +0530

    register loggin problems ok

---
+++ backend/middleware/Authorization.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+
+
+const generateToken = async()=>{
+
+}
+
+module.exports ={
+  generateToken
+}
--- backend/routes/User.routes.js
+++ backend/routes/User.routes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 
 const userController = require('../controllers/User.controller')
+const authMiddleware = require('../middleware/Authorization')
+
+
 
 //Account creation
 router.post("/signup",userController.signup)
